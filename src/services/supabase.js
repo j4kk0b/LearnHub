b754ff3beee7a3dcd94d0a94441407766431a556
j4kk0b <jakubkozaneckidev@gmail.com>
2024-06-16 02:03:47 +0200

    Last touches

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://ttknlranfprmimeixisy.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0a25scmFuZnBybWltZWl4aXN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0NDcyNTAsImV4cCI6MjAzNDAyMzI1MH0.MbkPYkfaGlgRdAWdzyCm29YcaEmnKWXZgrQpTSfqnJ0"
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_API_KEY
 );
 
 export default supabase;
