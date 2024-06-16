19da5fd0a3529303ca4bbbb6e944e83889988868
Jakub Kozanecki <102600319+j4kk0b@users.noreply.github.com>
2024-06-16 02:11:07 +0200

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_API_KEY
+  "https://ttknlranfprmimeixisy.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0a25scmFuZnBybWltZWl4aXN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0NDcyNTAsImV4cCI6MjAzNDAyMzI1MH0.MbkPYkfaGlgRdAWdzyCm29YcaEmnKWXZgrQpTSfqnJ0"
 );
 
 export default supabase;
+
